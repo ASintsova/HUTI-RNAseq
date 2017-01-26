be29a3f5e7a6be829de2f872faad181f35f8313a
ASintsova <annasint@umich.edu>
2017-01-26 17:06:17 -0500

    Edited the contig information in gff files by crossreferencing with original PROKKA gffs

--- code/get-homologs/get_gffs_from_homolgs.py
+++ code/get-homologs/get_gffs_from_homolgs.py
@@ -7,6 +7,7 @@ Created on Fri Jan  6 10:50:55 2017
 import sys
 filename_in = str(sys.argv[1])
 filename_out = str(sys.argv[2])
+funct = str(sys.argv[3])
 
 def getGFFs (filename_in, filename_out):
  
@@ -33,8 +34,62 @@ def getGFFs (filename_in, filename_out):
                 else:
                     strand = '.'
 
-                gff.write('{}\tcustom\tCDS\t{}\t{}\t.\t{}\t.\tgene_id "{}"; gene_name "{}"; PROKKA_ID "{}"\n'.format
+                gff.write('{}\tcustom\tCDS\t{}\t{}\t.\t{}\t.\tgene_id={}; gene_name={}; PROKKA_ID={}\n'.format
                       (genome, start, end, strand, gene_id, gene_name, prokka))     
                 #print ('{}\tcustom\tCDS\t{}\t{}\t.\t{}\t.\tgene_id "{}"; gene_name "{}"; PROKKA_ID "{}"\n'.format
                       #(genome, start, end, strand, gene_id, gene_name, prokka))
-getGFFs(filename_in, filename_out)
+
+
+def crossRefWithProkka (gh_gff, prokka_gff):
+    prokka = open(prokka_gff)
+    wrong_locs = []
+    wl = gh_gff[:-4] + "_wrong_locs.txt"
+    
+    wl_fn = open(wl, "w+")
+    
+    gh = open(gh_gff)
+    prokka.readline()# skip first line
+    prokka_ids_contig = {}
+    prokka_ids_locs = {}
+    fn = gh_gff[:-4] + "_final.gff"
+    final_gff = open(fn, "w+")
+    final_gff.write("#gff-veresion 3")
+    for line in prokka:
+        if line.startswith("##"):
+            break
+    
+        else:
+            words = line.rstrip().split("\t")
+            
+            prokka_id = words[8].split(";")[0].split("=")[1]
+            prokka_ids_contig[prokka_id]= words[0]
+            prokka_ids_locs[prokka_id] = (int(words[3]), int(words[4]))
+   
+    for line in gh:
+         
+        words = line.rstrip().rsplit("\t")
+        
+        
+        
+        id = words[8].split("PROKKA_ID=")[1]
+     
+        new_line = "\t".join(words[1:9])
+        
+       
+        if (int(words[3]), int(words[4])) != prokka_ids_locs[id]:
+            wrong_locs.append(id)
+        
+        final_gff.write(str(prokka_ids_contig[id])+"\t" +new_line + "\n")
+       
+    wl_fn.write(str(wrong_locs))
+    
+if funct == '1':
+    
+    getGFFs(filename_in, filename_out) 
+    
+elif funct == '2':
+    
+    crossRefWithProkka (filename_in, filename_out)
+else:
+    print ("Invalid Input")
+        
