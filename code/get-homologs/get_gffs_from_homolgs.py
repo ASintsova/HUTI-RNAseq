6cf5adfbe645c1e763fe72a01bd94603fb98dd1d
ASintsova <annasint@umich.edu>
2017-02-09 16:57:29 -0500

    By some stupidity I deleted the getGFF function, so now restored

--- code/get-homologs/get_gffs_from_homolgs.py
+++ code/get-homologs/get_gffs_from_homolgs.py
@@ -9,7 +9,35 @@ prokka_gff = str(sys.argv[1])
 gh_gff = str(sys.argv[2])
 #funct = str(sys.argv[3])
 
-
+def getGFFs (filename_in, filename_out):
+   
+     gff = open(filename_out, "w+")
+     with open (filename_in, 'r') as fh:
+         for line in fh:
+             if line.startswith(">>"):
+                 gene_id = line.rstrip()[2:]
+                 
+                 
+             elif line.startswith(">"):
+                 next_line = line
+                 description = next_line.split("|")
+                 genome = description[2] + "_1"
+                 gene_name = description[3]
+                 start = description[5].split(":")[1].split("-")[0]
+                 end = description[5].split(":")[1].split("-")[1]
+                 strand = description[5].split(":")[2].split("^^")[0].strip()
+                 prokka = description[0].split(":")[1]
+                 if strand == '1':
+                     strand = '+'
+                 elif strand == '-1':
+                     strand = '-'
+                 else:
+                      strand = '.'
+  
+                 gff.write('{}\tcustom\tCDS\t{}\t{}\t.\t{}\t.\tgene_id={}; gene_name={}; PROKKA_ID={}\n'.format
+                        (genome, start, end, strand, gene_id, gene_name, prokka))     
+                  #print ('{}\tcustom\tCDS\t{}\t{}\t.\t{}\t.\tgene_id "{}"; gene_name "{}"; PROKKA_ID "{}"\n'.format
+                        #(genome, start, end, strand, gene_id, gene_name, prokka))
     
 
 def getProkkaIds(prokka_gff):
