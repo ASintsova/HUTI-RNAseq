770b6da58f1c9c07bff7f23dcad605610425a242
ASintsova <annasint@umich.edu>
2018-08-30 14:44:37 -0400

    Finished (for now) refactoring correlations and PCA

--- analysis/methods/helpers.py
+++ analysis/methods/helpers.py
@@ -67,3 +67,16 @@ def timer(func):
         print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
         return value
     return wrapper_timer
+
+
+#######################################################################################
+# Make a container for each file time (gff, etc)
+
+class ReadVisits(object):
+    def __init__(self, data_path):
+        self.data_path = data_path
+
+    def __iter__(self):
+        with open(self.data_path) as f:
+            for line in f:
+                yield int(line)
--- analysis/methods/settings.py
+++ analysis/methods/settings.py
@@ -1,5 +1,6 @@
 import datetime as dt
 import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
 import matplotlib as mpl
 from matplotlib.colors import ListedColormap
 from matplotlib.colors import LinearSegmentedColormap
@@ -25,15 +26,19 @@ def get_labels(df):
     """ df.index needs to be in form strain_condition"""
     labels = []
     for i in df.index:
-        strain = i.split("_")[0]
-        cond = ur if i.split("_")[1] == "UR" else uti
-        label = sample_label(strain, cond)
+        label = sample_label_from_sample_name(i)
         labels.append(label)
     return labels
 
 
 def sample_label(strain, treat, jn=" | "):
     return strain + jn + treat + " "
+
+
+def sample_label_from_sample_name(sample_name):
+    strain = sample_name.split("_")[0]
+    condition = ur if sample_name.split("_")[1] == "UR" else uti
+    return sample_label(strain, condition)
 
 
 col1 = "#f34236"
@@ -54,3 +59,74 @@ strain_qual = {'good': ["HM56", "HM14", "HM43", "HM54", "HM86"],
                'okay': ["HM56", "HM14", "HM43", "HM54", "HM86", "HM01", "HM03", "HM06", "HM68"],
                'so-so': ["HM57", "HM17", "HM07" "HM60"],
                'bad': ["HM66"]}
+
+
+
+"""
+Plotting PCA elipses:
+
+__author__:
+
+"""
+
+def plot_point_cov(points, nstd=2, ax=None, **kwargs):
+    """
+    Plots an `nstd` sigma ellipse based on the mean and covariance of a point
+    "cloud" (points, an Nx2 array).
+
+    Parameters
+    ----------
+        points : An Nx2 array of the data points.
+        nstd : The radius of the ellipse in numbers of standard deviations.
+            Defaults to 2 standard deviations.
+        ax : The axis that the ellipse will be plotted on. Defaults to the
+            current axis.
+        Additional keyword arguments are pass on to the ellipse patch.
+
+    Returns
+    -------
+        A matplotlib ellipse artist
+    """
+    pos = points.mean(axis=0)
+    cov = np.cov(points, rowvar=False)
+    return plot_cov_ellipse(cov, pos, nstd, ax, **kwargs)
+
+
+def plot_cov_ellipse(cov, pos, nstd=2, ax=None, **kwargs):
+    """
+    Plots an `nstd` sigma error ellipse based on the specified covariance
+    matrix (`cov`). Additional keyword arguments are passed on to the
+    ellipse patch artist.
+
+    Parameters
+    ----------
+        cov : The 2x2 covariance matrix to base the ellipse on
+        pos : The location of the center of the ellipse. Expects a 2-element
+            sequence of [x0, y0].
+        nstd : The radius of the ellipse in numbers of standard deviations.
+            Defaults to 2 standard deviations.
+        ax : The axis that the ellipse will be plotted on. Defaults to the
+            current axis.
+        Additional keyword arguments are pass on to the ellipse patch.
+
+    Returns
+    -------
+        A matplotlib ellipse artist
+    """
+    def eigsorted(cov):
+        vals, vecs = np.linalg.eigh(cov)
+        order = vals.argsort()[::-1]
+        return vals[order], vecs[:,order]
+
+    if ax is None:
+        ax = plt.gca()
+
+    vals, vecs = eigsorted(cov)
+    theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
+
+    # Width and height are "full" widths, not radius
+    width, height = 2 * nstd * np.sqrt(vals)
+    ellip = Ellipse(xy=pos, width=width, height=height, angle=theta, **kwargs)
+
+    ax.add_artist(ellip)
+    return ellip
